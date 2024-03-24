b61807d8e48cd25d941f7439a81d5b346bad2cd8
Maksimdon1 <masterdigitalit@gmail.com>
2024-03-24 15:12:31 +0300

    env

--- src/utils/image-util.ts
+++ src/utils/image-util.ts
@@ -3,7 +3,7 @@ function getImageURL(name:string | undefined){
 	return  `http://192.168.1.3:3000/static/svg/${name}.svg`
 }
 if(import.meta.env.VITE_MODE === 'production'){
-	return  `https://svg-blog-server.onrender.com/static/svg/${name}.svg`
+	return  `https://svg-blog-server.onrender.com/static/svg/${name?.toLocaleLowerCase()}.svg`
 }
 }
 export default getImageURL
